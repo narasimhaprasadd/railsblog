b493c869c668cd45a39c0e275d5f2368e6344653
Narasimha Prasad <nprasad@thoughtworks.com>
2016-09-06 15:38:00 +0530

    able to list all the entries

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,20 @@ class ArticlesController < ApplicationController
 	def new 
 	end
 
+	def index
+		@articles = Article.all
+		return @articles
+	end
+	
 	def create
   		@article = Article.new(parameters) 
   		@article.save
-  		#redirect_to @article
+  		redirect_to @article
 	end
 
 	def show
 		@article  = Article.find(params[:id])
 	end
-
 
 	private def parameters
 		params.require(:article).permit(:title, :text)

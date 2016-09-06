a02c46b68944b7dbd3518012fc869fb95d0ba63a
Narasimha Prasad <nprasad@thoughtworks.com>
2016-09-06 14:06:38 +0530

    object getting saved in model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,22 @@ class ArticlesController < ApplicationController
 	def new 
 	end
 
-	def create 
+	def create
+  		@article = Article.new(parameters) 
+  		@article.save
+  		redirect_to @article
 	end
+
+	def show
+		@article  = Article.find(params[:id])
+	end
+
+
+	private def parameters
+		params.require(:article).permit(:title, :text)
+	end
+	
+
 	
 	
 end
